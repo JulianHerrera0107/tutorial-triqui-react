540cb988ff62485197b7a6f768b92a219c5aebb1
Julian-Herrera <juliandavih@gmail.com>
2024-12-17 11:52:07 -0500

    Agregando el prop Value en los componentes de Square con sus respectivos valores

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-function Square(){
-  return <button className="square">1</button>;
+function Square({ value }){
+  /* componente Square se le puede pasar un objeto llamado value*/
+  /* Creamos un componente Square para editar simultaneamente los elementos*/
+  return <button className="square">{value}</button>; //Utilizar las {} para llamar el componente
 }
 export default function Board() {
   return (
@@ -7,19 +9,19 @@ export default function Board() {
     /*Utilzamos Fragmentos (<> </>); para agregar más de un elemento JSX*/
       <div className="board-row">
         /* Si queremos dividir en secciones diferentes los elementos utilizamos un Div con estilos CSS */
-        <button className="square">1</button>;
-        <button className="square">2</button>;
-        <button className="square">3</button>;
+        <Square value="1"/>; //Componente value en square asignando un valor
+        <Square value="2"/>;
+        <Square value="3"/>;
       </div>
       <div className="board-row">
-        <button className="square">4</button>;
-        <button className="square">5</button>;
-        <button className="square">6</button>;
+        <Square value="4"/>;
+        <Square value="5"/>;
+        <Square value="6"/>;
       </div>
       <div className="board-row">
-        <button className="square">7</button>;
-        <button className="square">8</button>;
-        <button className="square">10</button>;
+        <Square value="7"/>;
+        <Square value="8"/>;
+        <Square value="9"/>;
       </div>
     </>
   );
