9422f7d0ebdb1261b447a3b8b4d692dd662f27e4
Julian-Herrera <juliandavih@gmail.com>
2024-12-17 12:15:33 -0500

    Realizando un componente interactivo con handleClick

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 function Square({ value }){
+  function handleClick(){
+    console.log('Hiciste click'); //Componente interactivo
+  }
   /* componente Square se le puede pasar un objeto llamado value*/
   /* Creamos un componente Square para editar simultaneamente los elementos*/
-  return <button className="square">{value}</button>; //Utilizar las {} para llamar el componente
+  return (
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+        {value}
+      </button>
+  ); //Utilizar las {} para llamar el componente
 }
 export default function Board() {
   return (
@@ -9,19 +19,19 @@ export default function Board() {
     /*Utilzamos Fragmentos (<> </>); para agregar más de un elemento JSX*/
       <div className="board-row">
         /* Si queremos dividir en secciones diferentes los elementos utilizamos un Div con estilos CSS */
-        <Square value="1"/>; //Componente value en square asignando un valor
-        <Square value="2"/>;
-        <Square value="3"/>;
+        <Square value="1"/> //Componente value en square asignando un valor
+        <Square value="2"/>
+        <Square value="3"/>
       </div>
       <div className="board-row">
-        <Square value="4"/>;
-        <Square value="5"/>;
-        <Square value="6"/>;
+        <Square value="4"/>
+        <Square value="5"/>
+        <Square value="6"/>
       </div>
       <div className="board-row">
-        <Square value="7"/>;
-        <Square value="8"/>;
-        <Square value="9"/>;
+        <Square value="7"/>
+        <Square value="8"/>
+        <Square value="9"/>
       </div>
     </>
   );
