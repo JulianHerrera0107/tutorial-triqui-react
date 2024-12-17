8fb475ccc392c21ac477f8c2c963740b90c55085
Julian-Herrera <juliandavih@gmail.com>
2024-12-17 17:32:14 -0500

    Agregando la función calcularGanador por medio de un texto que lo pueda indicar

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
-
+  
   function handleClick(i) { //Creación de un Closure para la función handleClick
-    if (squares[i]) { //Si el cuadrado ya está lleno, genera un return en la función handleClick, 
+    if (squares[i] || calcularGanador(squares)) { //Si el cuadrado ya está lleno, genera un return en la función handleClick, 
     // antes de que intente actualizar el estado del tablero.
       return;
     }
@@ -27,9 +27,18 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calcularGanador(squares);
+  let status;
+  if (winner) {
+    status = `El Ganador es ${winner}`;
+  } else {
+    status = `Siguiente jugador: "${xIsNext ? X : O}"`;
+  }
+
   return (
     <>
     /*Utilzamos Fragmentos (<> </>); para agregar más de un elemento JSX*/
+      <div className="status">{status}</div>
       <div className="board-row">
         /* Si queremos dividir en secciones diferentes los elementos utilizamos un Div con estilos CSS */
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} /> 
@@ -49,4 +58,23 @@ export default function Board() {
       </div>
     </>
   );
+}
+function calcularGanador(squares){
+  const lines = [
+    [0, 1, 2], //Gana en superior horizontal
+    [3, 4, 5], //Gana en centro horizontal
+    [6, 7, 8], //Gana en bajo horizontal
+    [0, 3, 6], //Gana en izquierda vertical
+    [1, 4, 7], //Gana en medio vertical
+    [2, 5, 8], //Gana en derecha vertical
+    [0, 4, 8], //Gana en diagonal izquierda
+    [2, 4, 6], //Gana en diagonal derecha
+  ];
+  for (let i = 0; i < lines.length; i++){
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
